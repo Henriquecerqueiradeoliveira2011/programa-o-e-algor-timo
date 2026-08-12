57a79be135ebe9ca6a95f505ff41805534f8aca0
Henriquecerqueiradeoliveira2011 <hcerqueiradeoliveira11@gmail.com>
2026-08-12 12:15:14 -0300

    Add files via upload

---
+++ Javapa/Main2.java
@@ -0,0 +1,47 @@
+import javax.swing.JOptionPane;
+public class Main2{
+    public static void main(String[] args) {
+        // Declaração da matriz A na ordem 2x2 (matriz quadrada)
+        int [][] A = {
+                {1, 4},
+                {2, 8}
+        };
+        //Declaração da matriz B na ordem 2x2 (matriz quadrada)
+        int [][] B = {
+                {3, 6},
+                {7, 9}
+        };
+        //Declaração da matriz C que receberá o resultado
+        int [][]C = new int[2][2];
+        for (int i = 0; i <2;i++){
+            for(int j = 0; j<2;j++){
+                //A subtração das matrizes
+                C[i][j] = A[i][j]-B[i][j];
+            }
+        }
+        //Exibe a matriz A
+        System.out.println("Matriz A: ");
+        for(int i = 0; i<2;i++){
+            for(int j = 0;j<2;j++){
+                System.out.print(A[i][j]+ "\t");
+            }
+            System.out.println();
+        };
+        //Exibe a matriz B
+        System.out.println("\n Matriz B: ");
+        for (int i = 0; i <2; i++){
+            for (int j=0;j<2;j++){
+                System.out.print(B[i][j]+"\t");
+            }
+            System.out.println();
+        }
+        //Exibe a matriz C
+        System.out.println("\n Matriz C: ");
+        for (int i = 0; i <2; i++){
+            for(int j=0;j<2;j++){
+                System.out.print(C[i][j]+ "\t");
+            }
+            System.out.println();
+        }
+    }
+}
