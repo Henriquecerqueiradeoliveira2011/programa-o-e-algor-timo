ecd7d259f1a40a75259ac67d59c551701286cabb
Henriquecerqueiradeoliveira2011 <hcerqueiradeoliveira11@gmail.com>
2026-08-12 12:27:31 -0300

    Add files via upload

--- Javapa/Main3.java
+++ Javapa/Main3.java
@@ -1,2 +1,25 @@
 public class Main3 {
+    public static void main (String[] args){
+        int [][] A = {
+                {10, 20, 30},
+                {40, 50 , 60}
+        };
+        int [][] B = {
+                {2, 4 , 6},
+                {8, 10, 12}
+        };
+        int[][] C = new int[3][3];
+        for (int i = 0; i<2; i++){
+            for(int j=0; j<2;j++){
+                C[i][j]= A[i][j]/B[i][j];
+                if(B[i][j]!= 0){
+                }else{
+                    System.out.println(
+                            "Erro: Divisão por zero na posição"
+                    )
+                }
+            }
+        }
+    }
+
 }
